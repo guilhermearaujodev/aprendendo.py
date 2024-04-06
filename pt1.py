df6aa64afd1b25efd58989696298a06ead55a6c1
Guilherme <guilherme.sajs@gmail.com>
2024-04-06 01:24:56 -0300

    first commit

---
+++ pt1.py
@@ -0,0 +1,4 @@
+ano_Nascimento = input('Favor informar o seu ano de nascimento: ')
+ano_Atual = input('Favor informar o seu ano atual: ')
+idade = int(ano_Atual) - int(ano_Nascimento)
+print(idade)
---
+++ pt3.py
@@ -0,0 +1,7 @@
+valor1 = int(input("Por favor, informe o primeiro valor: "))
+valor2 = int(input("Por favor, informe o segundo valor: " ))
+
+soma = valor1 + valor2
+
+print("A soma é {}".format(soma))
+print(f"A soma é {soma}")
