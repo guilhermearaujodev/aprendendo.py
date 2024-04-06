07900f479f3c8ccf3cf2609a85b294bbc473f3c7
Guilherme <guilherme.sajs@gmail.com>
2024-04-06 02:26:11 -0300

    create pt4.py

---
+++ pt4.py
@@ -0,0 +1,34 @@
+#verificar os tipos das variaveis
+
+x = 10
+y = 30.3
+z = "teste"
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#realizando a conversao de x valor inteiro 10
+print(x)
+x = float(x)
+print(x)
+print(type(x))
+
+#realizando a conversao de y valor float 30.3
+print(y)
+y = int(y)
+print(y)
+print(type(y))
+
+#len mostra a quantidade de caracteres que tem na variavel
+print(z)
+print(len(z))
+
+#upper mostra todas as letras maiusculas
+print(z.upper())
+
+#lower mostra todas as letras minusculas
+print(z.lower())
+
+#realizar divisões
+print(z.split("s"))
