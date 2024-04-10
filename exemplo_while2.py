7511be4ad7106dc70696c29e140997ace30d9648
Guilherme <guilherme.sajs@gmail.com>
2024-04-10 03:56:03 -0300

    while (segundo  exemplo)

---
+++ exemplo_while2.py
@@ -0,0 +1,6 @@
+i = 0
+#enquanto a variável contadora não chegar ao limite
+while (i<10):
+    #para cada uma das repetições uma mensagem é exibida
+    print("Mais uma mensagem, com i valendo: {}".format(i))
+    i = i + 1
