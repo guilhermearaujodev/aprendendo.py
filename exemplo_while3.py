569e114839e7fac6e31c011a820cf376343763c2
Guilherme <guilherme.sajs@gmail.com>
2024-04-10 04:25:26 -0300

    loop while (terceira exemplo)

---
+++ exemplo_while3.py
@@ -0,0 +1,6 @@
+numero = 1
+# se o resto da divisão for diferente de 0, vai repetir o loop
+while numero % 2 != 0:
+    numero = int(input("Por favor, informe um número par! "))
+
+    print("Parabéns, o número informado é par!")
