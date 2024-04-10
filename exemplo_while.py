0f4d6c2a3ce3434138ffe542da097676aa4d75c6
Guilherme <guilherme.sajs@gmail.com>
2024-04-10 03:51:08 -0300

    while

---
+++ exemplo_while.py
@@ -0,0 +1,6 @@
+resposta = "0"
+#enquanto a resposta não for 42, a repetição ocorre
+while (resposta != "42"):
+    resposta = input("Qual a resposta para a vida, o universo e tudo mais?")
+#quando o laço terminar, a mensagem é exibida.
+print("Parabéns, você acertou!")
