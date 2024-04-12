2879fb7ea14ad40d10bf2e5612f32ebf7d7eadfe
Guilherme <guilherme.sajs@gmail.com>
2024-04-12 17:52:38 -0300

    loop for e função range

---
+++ loop_for.py
@@ -0,0 +1,6 @@
+#a função range definirá o intervalo de valores para a variável
+
+# a variável contadora deverá assumir valores no intervalo entre 0 e 9
+for x in range(10):
+    #agora para cada m desses valores, vamos printar o valor da variável
+    print(x)
