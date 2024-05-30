e6ee88438a914c22101538bf185c0e0f1cb2810a
Guilherme <guilherme.sajs@gmail.com>
2024-05-30 02:24:11 -0300

    tuplas

---
+++ Modulos2/tuplas.py
@@ -0,0 +1,8 @@
+#as tuplas ou tuples são estruturas em Py com a caracteristica de serem imutaveis, os dados armazenados permanecerao identicos e na mesma posicao ao longo da execucao do programa
+
+categorias = ("aprendiz", "junior", "pleno", "senior")
+
+print(categorias[2])
+
+for categoria in categorias:
+    print(categoria)
